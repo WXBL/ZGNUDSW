37acde6302e30b86638fe47c4d8c5bec0b48e876
zhxOoO <2311326377@qq.com>
2016-04-27 09:38:27 +0800

    new

--- ZGNCDSW/ZGNCDSW/WXHomeViewController.m
+++ ZGNCDSW/ZGNCDSW/WXHomeViewController.m
@@ -10,7 +10,13 @@
 #import "WXSearchBar.h"
 #import "UIView+Extension.h"
 #import "PrefixHeader.pch"
-@interface WXHomeViewController ()
+
+@interface WXHomeViewController ()<UIScrollViewDelegate>
+
+// 创建一个用来引用计时器对象的属性
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic,strong)UIScrollView *scrollView;
+@property (nonatomic,strong)UIPageControl *pageControl;
 
 @property (nonatomic,strong)NSMutableArray *imagesURLStrings;
 @end
@@ -26,6 +32,51 @@
     return self;
 }
 
+// 实现UIScrollView的滚动方法
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    
+    // 如何计算当前滚动到了第几页？
+    // 1. 获取滚动的x方向的偏移值
+    CGFloat offsetX = scrollView.contentOffset.x;
+    // 用已经偏移了得值, 加上半页的宽度
+    offsetX = offsetX + (scrollView.frame.size.width * 0.5);
+    
+    // 2. 用x方向的偏移的值除以一张图片的宽度(每一页的宽度)，取商就是当前滚动到了第几页（索引）
+    int page = offsetX / scrollView.frame.size.width;
+    
+    // 3. 将页码设置给UIPageControl
+    self.pageControl.currentPage = page;
+    
+    //NSLog(@"滚了，要在这里根据当前的滚动来计算当前是第几页。");
+}
+
+// 实现即将开始拖拽的方法
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    // 停止计时器
+    // 调用invalidate一旦停止计时器, 那么这个计时器就不可再重用了。下次必须重新创建一个新的计时器对象。
+    [self.timer invalidate];
+    
+    // 因为当调用完毕invalidate方法以后, 这个计时器对象就已经废了，无法重用了。所以可以直接将self.timer设置为nil
+    self.timer = nil;
+}
+
+// 实现拖拽完毕的方法
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    
+    // 重新启动一个计时器
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(scrollImage) userInfo:nil repeats:YES];
+    
+    // 再次修改一下新创建的timer的优先级
+    // 修改self.timer的优先级与控件一样
+    // 获取当前的消息循环对象
+    NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
+    // 改变self.timer对象的优先级
+    [runLoop addTimer:self.timer forMode:NSRunLoopCommonModes];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
@@ -36,16 +87,107 @@
     
     self.navigationItem.titleView = searchBar;
     
-    
+    [self addScrollView];
 }
 
 
 #pragma mark -设置轮播图片
 -(void)addScrollView{
+    
+    UIScrollView *scrollView = [[UIScrollView alloc]init];
+    scrollView.frame = CGRectMake(0, 0, screenWidth, 200);
+    scrollView.backgroundColor = [UIColor grayColor];
+    self.tableView.tableHeaderView = scrollView;
+    self.scrollView = scrollView;
+    
+    CGFloat imgW = screenWidth;
+    CGFloat imgH = 200;
+    CGFloat imgY = 0;
+    
+    // 1. 循环创建5个UIImageView添加到ScrollView中
+    for (int i = 0; i < 4; i++) {
+        // 创建一个UIImageView
+        UIImageView *imgView = [[UIImageView alloc] init];
+        
+        // 设置UIImageView中的图片
+        NSString *imgName = [NSString stringWithFormat:@"%d.jpg", i + 1];
+        imgView.image = [UIImage imageNamed:imgName];
+        
+        // 计算每个UIImageView在UIScrollView中的x坐标值
+        CGFloat imgX = i * imgW;
+        // 设置imgView的frame
+        imgView.frame = CGRectMake(imgX, imgY, imgW, imgH);
+        
+        // 把imgView添加到UIScrollView中
+        [scrollView addSubview:imgView];
+    }
 
-    self.imagesURLStrings = [NSMutableArray arrayWithObjects:@"h1.jpg",@"h2.jpg",@"h3.jpg",@"h4.jpg", nil];
+    
+    // 设置UIScrollView的contentSize(内容的实际大小)
+    CGFloat maxW = screenWidth * 4;
+    self.scrollView.contentSize = CGSizeMake(maxW, 0);
+    
+    
+    // 实现UIScrollView的分页效果
+    // 当设置允许分页以后, UIScrollView会按照自身的宽度作为一页来进行分页。
+    self.scrollView.pagingEnabled = YES;
+    
+    // 隐藏水平滚动指示器
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    
+    self.pageControl = [[UIPageControl alloc]initWithFrame:CGRectMake(screenWidth *0.3, 160, screenWidth * 0.4, 40)];
+//    pageControl.backgroundColor = [UIColor redColor];
+    [self.pageControl setCurrentPageIndicatorTintColor:[UIColor colorWithRed:0.8 green:0.2 blue:0.3 alpha:1]];
+    [self.view addSubview:self.pageControl];
+    
+    // 指定UIPageControl的总页数
+    self.pageControl.numberOfPages = 4;
+    
+    // 指定默认是第0页
+    self.pageControl.currentPage = 0;
+    
+    
+    // 创建一个"计时器"控件NSTimer控件
+    // 通过scheduledTimerWithInterval这个方法创建的计时器控件, 创建好以后自动启动
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(scrollImage) userInfo:nil repeats:YES];
+    
+    // 修改self.timer的优先级与控件一样
+    // 获取当前的消息循环对象
+    NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
+    // 改变self.timer对象的优先级
+    [runLoop addTimer:self.timer forMode:NSRunLoopCommonModes];
+
+
     
 }
+
+// 自动滚动图片的方法
+// 因为在创建计时器的时候, 指定了时间间隔是1.0秒,所以下面这个方法每隔一秒钟执行一次
+- (void)scrollImage
+{
+    // 每次执行这个方法的时候, 都要让图片滚动到下一页
+    // 如何让UIScrollView滚动到下一页?
+    // 1. 获取当前的UIPageControl的页码
+    NSInteger page = self.pageControl.currentPage;
+    
+    // 2. 判断页码是否到了最后一页, 如果到了最后一页, 那么设置页码为0（回到第一页）。如果没有到达最后一页, 则让页码+1
+    if (page == self.pageControl.numberOfPages - 1) {
+        // 表示已经到达最后一页了
+        page = 0; // 回到第一页
+    } else {
+        page++;
+    }
+    // 3. 用每页的宽度 * (页码 + 1) == 计算除了下一页的contentOffset.x
+    CGFloat offsetX = page * self.scrollView.frame.size.width;
+    
+    // 4. 设置UIScrollView的contentOffset等于新的偏移的值
+    [self.scrollView setContentOffset:CGPointMake(offsetX, 0) animated:YES];
+    
+    
+    // 如果图片现在已经滚动到最后一页了, 那么就滚动到第一页
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -54,25 +196,30 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+
+    return 1;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+   
+    static NSString *cellStr = @"cell";
     
-    // Configure the cell...
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellStr];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellStr];
+    }
+    
+    cell.textLabel.text = @"ads";
     
     return cell;
 }
-*/
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 44;
+}
 
 /*
 // Override to support conditional editing of the table view.
--- ZGNCDSW/ZGNCDSW/WXUserModel.h
+++ ZGNCDSW/ZGNCDSW/WXUserModel.h
@@ -11,24 +11,4 @@
 @interface WXUserModel : NSObject
 @property(assign)int  UserID;
 @property(strong,nonatomic)NSString *UserName;
-<<<<<<< HEAD
-@property(strong,nonatomic)NSString *Password;
-@property(strong,nonatomic)NSString *Tel;
-@property(strong,nonatomic)NSString *Age;
-@property(strong,nonatomic)NSString *Sex;
-@property(strong,nonatomic)NSString *Regist_time;
-@property(strong,nonatomic)NSString *Last_login_time;
-@property(strong,nonatomic)NSString *User_image;
-@property(strong,nonatomic)NSString *Email;
-@property(strong,nonatomic)NSString *Login_address;
-@property(strong,nonatomic)NSString *Now_address;
-@property(strong,nonatomic)NSString *Note;
-@property(strong,nonatomic)NSString *Level_ID;
-@property(strong,nonatomic)NSString *Rank_Content;
-
-
--(id)getUserDataWithDictionaryJSON:(NSDictionary *)dict;
--(id)getUserDataWithArrayJSON:(NSArray *)array;
-=======
->>>>>>> origin/master
 @end
